79aa0b465c5660c387453d74e42aa22660edf09b
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-08-24 17:05:19 +0900

    紐付け

--- ColumbiaCameraForUIKit/ColumbiaCameraForUIKit/ViewController.swift
+++ ColumbiaCameraForUIKit/ColumbiaCameraForUIKit/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
+    @IBAction func startCamera(_ sender: Any) {
+    }
 }
 
