0daa992b2df1a3fd55a95ff8475c6d997827ed44
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-08-24 19:27:51 +0900

    SecondViewContrroller.swiftを追加

---
+++ ColumbiaCameraForUIKit/ColumbiaCameraForUIKit/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ColumbiaCameraForUIKit
+//
+//  Created by 山根大生 on 2020/08/24.
+//  Copyright © 2020 taisei. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
